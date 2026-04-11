49518493a3feb268e9f832f9f6a46e1c887a1a32
qh120411 <quanghuyy0411@gmail.com>
2026-04-11 16:37:50 +0700

    code Giangvien.cpp

---
+++ src/Giangvien.cpp
@@ -0,0 +1,65 @@
+#include<../include/Giangvien.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+Giangvien::Giangvien(string ten, string gt, string nsinh,
+                   string que, string sdt, string mail,
+                   string dch, string cccd, string magv,
+                   string gvbomon, string khoadaot, string hcvi,
+                   string chuc, string chuyenngh, tinhtrangGV tinhtranggv )
+            : Nguoi(hoten, gioitinh, namsinh, quequan, sodienthoai, email, diachi, socccd),
+              mgv(magv), bomon(gvbomon), khoa(khoadaot), hocvi(hcvi), chucvu(chuc), chuyennganh(chuyenngh), tinhtrang(tinhtranggv) {}
+
+Giangvien::~Giangvien(){}
+
+void Giangvien::setmgv(const string &magv) {
+    mgv = magv;
+}
+string Giangvien::getmgv() const {
+    return mgv;
+}
+
+void Giangvien::setbomon(const string &gvbomon) {
+    bomon = gvbomon;
+}
+string Giangvien::getmgv() const {
+    return bomon;
+}
+
+void Giangvien::setkhoa(const string &khoadaot) {
+    khoa = khoadaot;
+}
+string Giangvien::getkhoa() const {
+    return khoa;
+}
+
+void Giangvien::sethocvi(const string &hcvi) {
+    hocvi = hcvi;
+}
+string Giangvien::getmgv() const {
+    return hocvi;
+}
+
+void Giangvien::setchucvu(const string &chuc) {
+    chucvu = chuc;
+}
+string Giangvien::getmgv() const {
+    return chucvu;
+}
+
+void Giangvien::setchuyennganh(const string &chuyenngh) {
+    chuyennganh = chuyenngh;
+}
+string Giangvien::getchuyennganh() const {
+    return chuyennganh;
+}
+
+void Giangvien::settinhtranggv(tinhtrangGV tinhtranggv) {
+    tinhtrang = tinhtranggv;
+}
+tinhtrangGV Giangvien::gettinhtranggv() const {
+    return tinhtrang;
+}
+
+
