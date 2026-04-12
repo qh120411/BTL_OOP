d413a0308da1e0ce00081e02cbde650e27162ed8
junn-dev <hungnguyen.junn@gmail.com>
2026-04-12 17:12:39 +0700

    add validate day, finish class DangKyHoc

--- include/utils/InputHelper.h
+++ include/utils/InputHelper.h
@@ -5,9 +5,10 @@
 
 class InputHelper {
 public:
-    static int inputInt(const std::string& prompt, int minValue = -2147483647, int maxValue = 2147483647);
-    static double inputDouble(const std::string& prompt, double minValue = -1e18, double maxValue = 1e18);
-    static std::string inputLine(const std::string& prompt);
+    static int inputInt(const std::string&, int, int);
+    static double inputDouble(const std::string&, double, double);
+    static std::string inputLine(const std::string&);
+    static std::string inputDate(const std::string&);
 };
 
 #endif
