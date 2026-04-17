c34573d96717d714cf7774b325496ad84603a18f
qh120411 <quanghuyy0411@gmail.com>
2026-04-17 19:10:35 +0700

    test  main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
-#include "include/models/DangKyHoc.h"
+#include <iostream>
+#include "include/models/Taikhoan.h"
+
+using namespace std;
 
 int main() {
-    DangKyHoc mh;
-    mh.nhap();
-    mh.xuat();
+    TaiKhoan tk;
+
+    if (tk.checklog("C:/Users/Admin/Desktop/BTL_OOP/data/Taikhoan.csv")) {
+        cout << "Dang nhap thanh cong\n";
+        string role = tk.getrole();
+
+        if (role == "admin") {
+            cout << "Dang vao he thong admin\n";
+        }
+        else if (role == "gv") {
+            cout << "Dang vao he thong giang vien\n";
+        }
+        else if (role == "sv") {
+            cout << "Dang vao he thong sinh vien\n";
+        }
+    }
+    else {
+        cout << "Sai tai khoan hoac mat khau\n";
+    }
 
     return 0;
 }
