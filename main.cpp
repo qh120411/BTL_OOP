e6863b6d4b49c0ae817586ccbdbc9983123a7fd5
qh120411 <quanghuyy0411@gmail.com>
2026-04-18 17:44:59 +0700

    add loop

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include "include/models/Taikhoan.h"
+#include "include/services/Menu.h"
 
 using namespace std;
 
 int main() {
     TaiKhoan tk;
+    while (true) {
+        if (tk.checklog("C:/Users/Admin/Desktop/BTL_OOP/data/Taikhoan.csv")) {
+            cout << "Dang nhap thanh cong\n";
+            string role = tk.getrole();
 
-    if (tk.checklog("C:/Users/Admin/Desktop/BTL_OOP/data/Taikhoan.csv")) {
-        cout << "Dang nhap thanh cong\n";
-        string role = tk.getrole();
-
-        if (role == "admin") {
-            cout << "Dang vao he thong admin\n";
+            if (role == "admin") {
+                menuAdmin();
+            }
+            else if (role == "gv") {
+                menuGiangVien();
+            }
+            else if (role == "sv") {
+                menuSinhVien();
+            }
         }
-        else if (role == "gv") {
-            cout << "Dang vao he thong giang vien\n";
-        }
-        else if (role == "sv") {
-            cout << "Dang vao he thong sinh vien\n";
-        }
-    }
-    else {
-        cout << "Sai tai khoan hoac mat khau\n";
     }
 
     return 0;
